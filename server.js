aa06a61d807dd2b724e336fc92e0b10c78fa3e9b
Ayman Adel <aymanadel003@gmail.com>
2016-04-10 18:31:33 +0200

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -20,26 +20,53 @@ app.get('/', function(request, response) {
 
 //GET /todos
 app.get('/todos', function(request, response) {
-    var queryParams = request.query;
-    var filteredTodos = todos;
+    var query = request.query;
 
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {
-            completed: true
-        });
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {
-            completed: false
-        });
+    //version 2
+    var where = {};
+
+    if (query.hasOwnProperty('completed') && query.completed == 'true') {
+        where.completed = true;
+
+    } else if (query.hasOwnProperty('completed') && query.completed == 'false') {
+        where.completed = false;
     }
 
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function(todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
+
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description.toLowerCase() = {
+            $like: '%' + query.q.toLowerCase() + '%'
+        };
     }
 
-    response.json(filteredTodos);
+    db.todo.findAll({
+        where: where
+    }).then(function(todos) {
+        response.json(todos);
+    }, function() {
+        response.status(500).send();
+    });
+
+    //version 1
+    // var filteredTodos = todos;
+
+    // if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    //     filteredTodos = _.where(filteredTodos, {
+    //         completed: true
+    //     });
+    // } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    //     filteredTodos = _.where(filteredTodos, {
+    //         completed: false
+    //     });
+    // }
+
+    // if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    //     filteredTodos = _.filter(filteredTodos, function(todo) {
+    //         return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    //     });
+    // }
+
+    // response.json(filteredTodos);
 });
 
 
